5e2a12d2c01a6bb564a9d4485cdf489919b58434
Jose Augusto <joseaugusto0408@gmail.com>
2026-06-17 12:29:56 -0300

    Passando parâmetro para a rota

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,11 @@ export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Ola, Expo Router 😁</Text>
-      <Button title="target" onPress={() => router.navigate("/Target")} />
+      <Button title="Nova meta" onPress={() => router.navigate("/Target")} />
+      <Button
+        title="Transação"
+        onPress={() => router.navigate("/transaction/123")}
+      />
     </View>
   );
 }
---
+++ src/app/transaction/[id].tsx
@@ -0,0 +1,12 @@
+import { router, useLocalSearchParams } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+export default function Transaction() {
+  const params = useLocalSearchParams<{ id: string }>();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 22 }}>ID: {params.id}</Text>
+      <Button title="Voltar" onPress={() => router.back()} />
+    </View>
+  );
+}
