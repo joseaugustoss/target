78614b95cee8c3e9da3bf01403e81da30972edd6
José Auguso <joseaugusto0408@gmail.como>
2026-06-25 11:03:36 -0300

    Unificando importação dos tokens de cores e fontes

--- src/components/HomeHeader/styles.ts
+++ src/components/HomeHeader/styles.ts
@@ -1,7 +1,17 @@
 import { StyleSheet } from "react-native";
+import { colors, fontFamily } from "@/theme";
 export const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: 324,
+    paddingHorizontal: 24,
+    justifyContent: "flex-end",
+  },
+  label: {
+    fontSize: 12,
+    color: colors.white,
+    fontFamily: fontFamily.regular,
+    paddingBottom: 18,
+    gap: 24,
   },
 });
